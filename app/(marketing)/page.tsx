adbe69223f6eb31c40c4bd782ffae981296d4824
Patric Vinicios <psdev7@gmail.com>
2023-10-24 01:29:24 -0300

    feat: home page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,9 +1,17 @@
-import { Button } from "@/components/ui/button"
+import { Heading } from "./_components/heading"
+import { Heroes } from "./_components/heroes"
+import { Footer } from "./_components/footer"
 
-export default function Home() {
+const MarketingPage = () => {
   return (
-    <>
-      <Button>Click me</Button>
-    </>
+    <div className="min-h-full flex flex-col">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        <Heading />
+        <Heroes />
+      </div>
+      <Footer />
+    </div>
   )
 }
+
+export default MarketingPage
